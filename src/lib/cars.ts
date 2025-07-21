19b182504ee181e0de2ebd105437c24e4f2bbc8f
Edier Migadalia <ediermigadalia@gmail.com>
2025-07-21 14:22:52 +0000

    agrega otro auto Bestune T55  precio 140 rebajalo a 125 Seguro del Auto

--- src/lib/cars.ts
+++ src/lib/cars.ts
@@ -177,5 +177,39 @@ export const cars: Car[] = [
         'Costo de combustible (única vez): $78.00 USD.'
       ]
     }
+  },
+  {
+    id: 9,
+    name: 'Bestune T55',
+    description: 'Un SUV elegante y bien equipado, que ofrece una experiencia de conducción superior.',
+    pricePerDay: 125,
+    originalPricePerDay: 140,
+    imageUrl: '/images/images/Bestune T55.jpg',
+    imageHint: 'suv gray',
+    features: ['4 Puertas', '5 Pasajeros', 'Automático', 'Gasolina', 'Clima', 'Maletero', 'Seguro incluido', 'Chapa ESA'],
+    details: {
+      additionalCharges: [
+        'Los cargos adicionales son abonados directamente a la compañía rentadora mediante tarjeta en el momento a la apertura del contrato. No se admitirán pagos en efectivo.'
+      ],
+      notIncluded: [
+        'Conductor adicional (Max. 2, precio por chofer por día: 3.50 USD).',
+        'Depósito de garantía.'
+      ],
+      included: [
+        'Seguro diario.',
+        'Depósito de Combustible.',
+        'Tasa de Aeropuerto (20.00 USD si aplica).'
+      ],
+      pickupAndDropoff: [
+        'Si su hora de DEVOLUCIÓN al momento de reservar es más de 4 horas que su hora de RECOGIDA el sistema cargara 1 día adicional a su reserva automáticamente. Asegúrese de entregar el auto a la hora establecida.'
+      ],
+      notes: [
+        'A conductores entre 21-24 años y 75-80 años de edad se le incrementará el costo del seguro en un 50%.',
+        'RentCubaCar solo confirma categorías, no marcas y/o modelos específicos.',
+        'Mínimo de renta: 5 días.',
+        'Costo de seguro por día: $30.00 USD.',
+        'Costo de combustible (única vez): $75.00 USD.'
+      ]
+    }
   }
 ];
